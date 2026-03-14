153e1087f8700fc5527e835ade1ce346d5e4afaf
Devananda09 <devananda0926@gmail.com>
2026-03-14 12:21:23 +0530

    Time: 0 ms (100%), Space: 12.5 MB (49.18%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def climbStairs(self, n):
+       step1=1
+       step2=1
+       
+       for i in range(n-1):
+        temp=step1
+        step1 = step1+step2
+        step2=temp
+       return step1
+        
