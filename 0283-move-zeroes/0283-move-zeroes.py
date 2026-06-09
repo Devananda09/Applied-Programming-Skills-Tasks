c32dad777539050a275b6b51c9e93c5dea0d070a
Devananda09 <devananda0926@gmail.com>
2026-06-09 15:27:16 +0530

    Time: 763 ms (6.4%), Space: 13.2 MB (97.18%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def moveZeroes(self, nums):
+        for i in nums:
+            if i==0:
+                nums.remove(i)
+                nums.append(i)
+        return nums
+        
